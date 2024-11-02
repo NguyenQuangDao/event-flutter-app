6f490888810348a6f5abe2cf14fd3b129a88f69d
NguyenQuangDao <anhdao2292003cxz@gmail.com>
2024-11-02 14:53:17 +0700

    'ud'

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:event_app/event/event_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        Locale('en'),
+        Locale('vi'),
+      ],
+      locale: Locale('vi'),
+      home: EventView(),
+    );
+  }
+}
